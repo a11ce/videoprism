d7e3d067915a4d09bfacf33e8b20bca1bc2a4c21
a11ce <a11ce@protonmail.com>
2018-10-12 15:13:56 -0400

    Progress bars and decimal coefficients

--- slice.py
+++ slice.py
@@ -3,18 +3,20 @@
 import imageio
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 
 def main():
     #TODO: arg this
-    path = "okgoscaled.mov"
+    path = "videos/img2.mov"
     videoPrism = loadVideo(path)
-    projectionSlice(videoPrism,2,1,0.2)
+    projectionSlice(videoPrism,0.1,0.1,0.1)
 
 def loadVideo(videoPath):
     firstFrame = True
     reader = imageio.get_reader(videoPath)
+    nFrames = reader.get_meta_data()['nframes'];
     framesArr = []
-    while True:
+    for _ in tqdm(range(nFrames)):
         try:
             frame = reader.get_next_data()
         #This occurs at the end of the file
@@ -30,12 +32,13 @@ def loadVideo(videoPath):
 
 def projectionSlice(videoPrism, xc,yc,c):
     c = int(map(c,0.0,1.0,0,videoPrism.shape[0]))
-    print(videoPrism.shape)
-    print(c)
+    #print(videoPrism.shape)
+    #print(c)
     projection = np.zeros(shape = (videoPrism.shape[2],videoPrism.shape[1],3))
-    for x in range(videoPrism.shape[2]):
-        for y in range(videoPrism.shape[1]):
-            z = (xc * x) + (yc * y) + c
+    for x in tqdm(range(videoPrism.shape[2])):
+        for y in tqdm(range(videoPrism.shape[1])):
+            z = int ((xc * x) + (yc * y) + c)
+            #print(z)
             try:
                 projection[x,y] = videoPrism[z,y,x]
             except:
@@ -43,8 +46,8 @@ def projectionSlice(videoPrism, xc,yc,c):
                 continue
     
     projection = projection.astype(np.uint8)
-    print(projection.shape)
-    print(projection[100,10])
+    #print(projection.shape)
+    #print(projection[100,10])
     img = Image.fromarray(projection,'RGB')
     img.show()
     
