3930f8b30b23154b36b18c9e929418d4e20fe78b
a11ce <a11ce@protonmail.com>
2019-11-04 21:28:55 -0500

    Use YAPF

--- movingSlice.py
+++ movingSlice.py
@@ -7,16 +7,23 @@ import slice
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="relative path to the video file")
-    parser.add_argument("-o","--output", type = str, help="relative path to output directory", required = False, default = "")
-    parser.add_argument("xc", help ="x-coefficient", type=float)
-    parser.add_argument("yc", help ="y-coefficient", type=float)
+    parser.add_argument("-o",
+                        "--output",
+                        type=str,
+                        help="relative path to output directory",
+                        required=False,
+                        default="")
+    parser.add_argument("xc", help="x-coefficient", type=float)
+    parser.add_argument("yc", help="y-coefficient", type=float)
     args = parser.parse_args()
 
     videoPrism = slice.loadVideo(args.path)
 
-    for c in tqdm(np.linspace(0,1,videoPrism.shape[0]), desc = "Slice Movement"):
+    for c in tqdm(np.linspace(0, 1, videoPrism.shape[0]),
+                  desc="Slice Movement"):
         img = slice.projectionSlice(videoPrism, args.xc, args.yc, c)
         slice.saveImage(img, args.xc, args.yc, c, args.path, args.output)
+
 
 if __name__ == "__main__":
     main()
--- slice.py
+++ slice.py
@@ -6,23 +6,25 @@ from PIL import Image
 from tqdm import tqdm
 import os
 
+
 def main():
     XC = -0.1
     YC = 0.1
-    C  = 0.6
+    C = 0.6
     FILENAME = "cat.mp4"
     #TODO: arg this
     path = "videos/" + FILENAME
     videoPrism = loadVideo(path)
 
-    randomProjectionSlice(videoPrism,path,100, "output/")
+    randomProjectionSlice(videoPrism, path, 100, "output/")
+
 
 def loadVideo(videoPath):
     firstFrame = True
     reader = imageio.get_reader(videoPath)
-    nFrames = reader.get_meta_data()['nframes'];
+    nFrames = reader.get_meta_data()['nframes']
     framesArr = []
-    for _ in tqdm(range(nFrames),desc = "Loading video (frames)"):
+    for _ in tqdm(range(nFrames), desc="Loading video (frames)"):
         try:
             frame = reader.get_next_data()
         #This occurs at the end of the file
@@ -30,59 +32,64 @@ def loadVideo(videoPath):
             break
         else:
             framesArr.append(np.asarray(frame))
-            
+
     #print(framesArr)
-    prism = np.stack(framesArr,axis = 0)
+    prism = np.stack(framesArr, axis=0)
     return prism
 
-def projectionSlice(videoPrism, xc,yc,c):
-    c = int(c * videoPrism.shape[0])  
+
+def projectionSlice(videoPrism, xc, yc, c):
+    c = int(c * videoPrism.shape[0])
     #c = int(map(c,0.0,1.0,0,videoPrism.shape[0]))
     #print(videoPrism.shape)
     #print(c)
-    projection = np.zeros(shape = (videoPrism.shape[2],videoPrism.shape[1],3))
-    for x in tqdm(range(videoPrism.shape[2]), desc = "Slicing video (x)"):
+    projection = np.zeros(shape=(videoPrism.shape[2], videoPrism.shape[1], 3))
+    for x in tqdm(range(videoPrism.shape[2]), desc="Slicing video (x)"):
         for y in range(videoPrism.shape[1]):
-            z = int ((xc * x) + (yc * y) + c)
+            z = int((xc * x) + (yc * y) + c)
             #print(z)
             try:
-                projection[x,y] = videoPrism[z,y,x]
+                projection[x, y] = videoPrism[z, y, x]
             except:
                 #print("out of bounds")
                 continue
-    
+
     #projection = projection.astype(np.uint8)
     #print(projection.shape)
     #print(projection[100,10])
-    img = Image.fromarray(projection.astype(np.uint8),'RGB')
+    img = Image.fromarray(projection.astype(np.uint8), 'RGB')
     img = img.transpose(Image.ROTATE_270)
     img = img.transpose(Image.FLIP_LEFT_RIGHT)
     return img
 
-def saveImage(img,xc,yc,c,fileName,outputDir):
+
+def saveImage(img, xc, yc, c, fileName, outputDir):
     endOfName = fileName.split("/")[-1]
     #print("end of name is " +endOfName)
     folder = (outputDir + endOfName + "/")
     os.makedirs(folder, exist_ok=True)
-    img.save(folder  + str(endOfName) +"."+  str(xc) +"."+str(yc) + "."+str(c) + ".png" )
+    img.save(folder + str(endOfName) + "." + str(xc) + "." + str(yc) + "." +
+             str(c) + ".png")
 
-def randomProjectionSlice(videoPrism,fileName, num, outputDir):
+
+def randomProjectionSlice(videoPrism, fileName, num, outputDir):
     nFrames = videoPrism.shape[0]
-    sideLength = (videoPrism.shape[1] + videoPrism.shape[2])/2
-    upBound = (nFrames / (sideLength*4))
+    sideLength = (videoPrism.shape[1] + videoPrism.shape[2]) / 2
+    upBound = (nFrames / (sideLength * 4))
     lowBound = -1 * upBound
     print("bound is " + str(upBound))
-    
-    for _ in tqdm(range(num), desc = "Random projections"):
-        xc = np.random.uniform(lowBound,upBound)
-        yc = np.random.uniform(lowBound,upBound)
-        c  = np.random.uniform((upBound/4),(1-(upBound/4)))
-        img = projectionSlice(videoPrism, xc, yc, c)
-        saveImage(img,xc,yc,c,fileName,outputDir)
-        
 
-def map( x,  in_min,  in_max,  out_min,  out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    for _ in tqdm(range(num), desc="Random projections"):
+        xc = np.random.uniform(lowBound, upBound)
+        yc = np.random.uniform(lowBound, upBound)
+        c = np.random.uniform((upBound / 4), (1 - (upBound / 4)))
+        img = projectionSlice(videoPrism, xc, yc, c)
+        saveImage(img, xc, yc, c, fileName, outputDir)
+
+
+def map(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
 
 if __name__ == "__main__":
     main()
